50aad8969eda1b52860e1ad0604382e80e311489
Ignat Vilesov <ignat.vilesov@gmail.com>
2022-10-19 17:31:32 +0300

    fix: resolve issues with imports in the code (#8)

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 import babel from '@rollup/plugin-babel'
 import typescript from '@rollup/plugin-typescript'
 
@@ -11,10 +12,11 @@ export default {
   },
   plugins: [
     resolve(),
+    commonjs(),
+    typescript(),
     babel({
       extensions: ['.ts'],
       babelHelpers: 'bundled',
     }),
-    typescript(),
   ],
 }
